d719213b504e2a09a0c5cb548aa1fef66714ab50
Miguel Palhas <mpalhas@gmail.com>
2019-12-04 18:19:25 +0000

    Fixes git tag usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,15 @@ output=task_definition.json
 
 template=$1
 dictionary=$2
-image=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$3:$GITHUB_SHA
+tag=$3
+parsed_tag=$(echo $tag | sed 's|.*/||')
+image=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$3:$parsed_tag
 
 /gen.py \
   --template $template \
   --dictionary $dictionary \
   -a DOCKER_IMAGE=$image \
-  -a GIT_HASH=$GITHUB_SHA \
+  -a GIT_HASH=$parsed_tag \
   > $output
 
 echo ::set-output name=file::$output
