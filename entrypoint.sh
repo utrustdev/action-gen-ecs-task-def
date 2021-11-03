b9ec9c95824415bcd88affec431b7378c2dbe602
Mário Silva <mario.silva@utrust.com>
2021-11-03 11:37:14 +0000

    Output filename generated based on `template` path to avoid overwritting the output when the action is used 1+ times on the same workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -ue
 
-output=task_definition.json
-
 template=$1
 dictionary=$2
 image=$3
 tag=$4
 parsed_tag=$(echo $tag | sed 's|.*/||')
 image=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$image:$parsed_tag
+
+template_no_slashes=$(echo $template | sed 's/\//-/g')
+output="output-${template_no_slashes}.json"
 
 /gen.py \
   --template $template \
